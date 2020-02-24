0bc824330699306f04d1c4abbf19fb216fea7f21
syu-y <syu12580526@gmail.com>
2020-02-24 16:35:55 +0900

    ダイアログに入力フォーム追加

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -1,12 +1,15 @@
 import AddScheduleDialog from './presentation';
 import { connect } from 'react-redux';
-import { addScheduleCloseDialog } from '../../redux/addSchedule/actions';
+import { addScheduleSetValue, addScheduleCloseDialog } from '../../redux/addSchedule/actions';
 
 const mapStateToProps = (state) => ({ schedule: state.addSchedule });
 
 const  mapDispatchToProps = (dispatch) => ({
   closeDialog: () => {
     dispatch(addScheduleCloseDialog());
+  },
+  setSchedule: (value) => {
+    dispatch(addScheduleSetValue(value));
   }
 });
 
