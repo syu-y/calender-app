bf8f5a90ee5d1ca238c6d8f5213983a9369bcd4f
syu-y <syu12580526@gmail.com>
2020-05-16 17:42:14 +0900

    予定詳細用のRedux

--- front/src/redux/currentSchedule/reducer.js
+++ front/src/redux/currentSchedule/reducer.js
@@ -1,7 +1,28 @@
-import {  } from 'redux/addSchedule/actions'
+import {
+  CURRENT_SCHEDULE_SET_ITEM,
+  CURRENT_SCHEDULE_OPEN_DIALOG,
+  CURRENT_SCHEDULE_CLOSE_DIALOG,
+} from 'redux/currentSchedule/actions';
 
 // 予定詳細初期値
 const init = {
   item: null,
-  isDialogOpen: false
-}
+  isDialogOpen: false,
+};
+
+const currentScheduleReducer = (state = init, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CURRENT_SCHEDULE_SET_ITEM:
+      return { ...state, item: payload };
+    case CURRENT_SCHEDULE_OPEN_DIALOG:
+      return { ...state, isDialogOpen: true };
+    case CURRENT_SCHEDULE_CLOSE_DIALOG:
+      return { ...state, isDialogOpen: false };
+    default:
+      return state;
+  }
+};
+
+export default currentScheduleReducer;
--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,12 +1,14 @@
-import { combineReducers } from "redux";
-import calendarReducer from "redux/calendar/reducer";
-import addScheduleReducer from "redux/addSchedule/reducer";
-import schedulesReducer from "redux/schedules/reducer";
+import { combineReducers } from 'redux';
+import calendarReducer from 'redux/calendar/reducer';
+import addScheduleReducer from 'redux/addSchedule/reducer';
+import schedulesReducer from 'redux/schedules/reducer';
+import currentScheduleReducer from 'redux/currentSchedule/reducer';
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
   addSchedule: addScheduleReducer,
-  schedules: schedulesReducer
+  schedules: schedulesReducer,
+  currentSchedule: currentScheduleReducer,
 });
 
 export default rootReducer;
